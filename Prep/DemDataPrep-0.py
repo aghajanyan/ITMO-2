f383b23667ee069dc7d3bd7a916b987244fb7b5c
Albert Aghajanyan <49070457+aghajanyan@users.noreply.github.com>
2024-02-22 13:44:23 +0300

    First extrapol try
    Method of abs rise

--- Prep/DemDataPrep-0.py
+++ Prep/DemDataPrep-0.py
@@ -2,16 +2,35 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = pd.read_excel("data0.xlsx", sheet_name=0)
 
-a = []
+class DemForecasting:
+    def ExtAvgRiseAbs(olddata, cycles):
+        inc = 0
+        for i in range(1, len(olddata)):
+            inc += olddata[i] - olddata[i - 1]
+
+        inc = inc / len(olddata)
+        for i in range(cycles):
+            olddata.append(olddata[len(olddata) - 1] + inc)
+
+data = pd.read_excel("data0.xlsx", sheet_name=3)
+
+districtdata = []
 year = []
 for j in range(1, data.shape[1]):
-    a.append(data.iloc[1, j])
+    districtdata.append(data.iloc[1, j])
     year.append(data.iloc[0, j])
 
-plt.plot(year, a, '.', color='black', markersize=7)
-plt.plot(year, a, color='black')
+n = 5
+DemForecasting.ExtAvgRiseAbs(districtdata, n)
+
+for i in range(n):
+    year.append(year[len(year) - 1] + 1)
+
+plt.plot(year, districtdata, '.', color='black', markersize=7)
+plt.plot(year[:len(districtdata) - n], districtdata[:len(districtdata) - n], color='blue', label='РОССТАТ')
+plt.plot(year[len(districtdata) - n - 1:], districtdata[len(districtdata) - n - 1:], color='red', label='Прогноз')
+plt.legend(loc='upper left')
 plt.xlabel("Год")
 plt.ylabel("Численность населения")
 plt.title(data.columns[0])
