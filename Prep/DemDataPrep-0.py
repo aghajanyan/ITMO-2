f69203eb3e8f0ee3b4a4006bcf04f8521b277d67
Albert Aghajanyan <49070457+aghajanyan@users.noreply.github.com>
2024-02-22 18:45:39 +0300

    Weighted extrap

--- Prep/DemDataPrep-0.py
+++ Prep/DemDataPrep-0.py
@@ -13,7 +13,16 @@ class DemForecasting:
         for i in range(cycles):
             olddata.append(olddata[len(olddata) - 1] + inc)
 
-data = pd.read_excel("data0.xlsx", sheet_name=3)
+    def ExtAvgRise(olddata, cycles):
+        inc = 0
+        for i in range(1, len(olddata)):
+            inc += (olddata[i] / olddata[i - 1]) - 1
+
+        inc = inc / len(olddata)
+        for i in range(cycles):
+            olddata.append(olddata[len(olddata) - 1] * (inc + 1))
+
+data = pd.read_excel("data0.xlsx", sheet_name=0)
 
 districtdata = []
 year = []
@@ -22,7 +31,7 @@ for j in range(1, data.shape[1]):
     year.append(data.iloc[0, j])
 
 n = 5
-DemForecasting.ExtAvgRiseAbs(districtdata, n)
+DemForecasting.ExtAvgRise(districtdata, n)
 
 for i in range(n):
     year.append(year[len(year) - 1] + 1)
