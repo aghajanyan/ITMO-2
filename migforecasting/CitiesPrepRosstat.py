e07e56282486bc78d3cab57fb199ef42daa12025
Albert Aghajanyan <49070457+aghajanyan@users.noreply.github.com>
2024-03-24 15:34:05 +0300

    Factories capacity calc.

--- migforecasting/CitiesPrepRosstat.py
+++ migforecasting/CitiesPrepRosstat.py
@@ -49,15 +49,26 @@ saldo = data.iloc[13, 1]
 saldo = ''.join(saldo.split())
 print(int(saldo))
 
-tmp = []
 examples = []
+cityname = data.iloc[1, 1]
+for m in range(1, data.shape[1]):
+    # вычисление суммы промышленного оборота (+ корр. строки в эксель)
+    factorycap = 0
+    for i in range(58, 62):
+        try:
+            data.iloc[i, m] = ''.join(data.iloc[i, m].split())
+            factorycap += float(data.iloc[i, m].replace(",", "."))
+        except (ValueError, TypeError):
+            factorycap += 0
+        except AttributeError:
+            factorycap += data.iloc[i, m]
 
-for k in range(7):
-    for j in range(1, data.shape[1]):
-        tmp.append(data.iloc[1, 1])
-        tmp.append(data.)
+    if data.iloc[1, m] == data.iloc[1, m]:  # если не NAN, то город меняется
+        cityname = data.iloc[1, m]
 
-
-
+    examples.append(City(cityname, data.iloc[4, m], data.iloc[15, m], data.iloc[17, m], data.iloc[19, m],
+                         data.iloc[22, m], data.iloc[23, m], data.iloc[27, m], data.iloc[34, m], data.iloc[38, m],
+                         data.iloc[45, m], data.iloc[52, m], data.iloc[55, m], factorycap, data.iloc[63, m],
+                         data.iloc[64, m], data.iloc[65, m], data.iloc[72, m], data.iloc[74, m], data.iloc[13, m]))
 
 
