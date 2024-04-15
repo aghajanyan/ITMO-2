ce8eac19cda2a2d4e41127cbada2af8d4172dc8c
Albert Aghajanyan <49070457+aghajanyan@users.noreply.github.com>
2024-04-15 10:22:42 +0300

    Neural network model for mig. forecasting

---
+++ migforecasting/MigrationRevealer (NN).py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 15 09:32:23 2024
+
+@author: Albert
+"""
+
+import csv
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_percentage_error
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import MinMaxScaler
+
+import tensorflow as tf
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dropout
+
+
+# Получение данных
+rawdata = pd.read_csv("citiesdataset-0.csv")
+rawdata = np.array(rawdata)
+
+np.random.shuffle(rawdata)
+
+# разбиение датасета на входные признаки и выходной результат (сальдо) 
+datasetin = rawdata[:, :18]
+datasetout = rawdata[:, 18:]
+
+# разбиение на обучающую и тестовую выборку
+trainin, trainout, testin, testout = [], [], [], []
+
+spliter = len(datasetin) * 0.9
+trainin = np.array(datasetin[:int(spliter)])
+trainout = np.array(datasetout[:int(spliter)])
+
+testin = np.array(datasetin[int(spliter):])
+testout = np.array(datasetout[int(spliter):])
+
+#модель
+model = Sequential()
+model.add(Dense(64, input_dim=18, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(1))
+
+model.compile(optimizer='adam', loss=tf.keras.losses.MeanAbsoluteError())
+history = model.fit(trainin, trainout, epochs=150, batch_size=6)
+
+pred = model.predict(trainin)
+pred1 = model.predict(testin)
+
+plt.plot(pred[:100], label='Предсказание')
+plt.plot(testout[:100], label='Реальное значение')
+plt.legend(loc='upper left')
+plt.xlabel("Номер теста")
+plt.ylabel("Объем продаж")
+plt.title("Прогноз на тестовой выборке")
+plt.show()
+
+plt.plot(history.history['loss'])
+plt.title("Процесс обучения")
+plt.xlabel("Номер эпохи")
+plt.ylabel("Оценка отклонения")
+plt.show()
+
+trainloss = mean_absolute_percentage_error(trainout, pred)
+testloss = mean_absolute_percentage_error(testout, pred1)
+
+print("MAPE on traning set: ", trainloss)
+print("MAPE on testing set: ", testloss)
+
