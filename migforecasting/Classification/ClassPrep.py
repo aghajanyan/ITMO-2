325098fac6862f0019dee17eeac7310b364a985c
Albert Aghajanyan <49070457+aghajanyan@users.noreply.github.com>
2024-05-21 12:46:28 +0300

    New KZ datasets for classification

--- migforecasting/Classification/ClassPrep.py
+++ migforecasting/Classification/ClassPrep.py
@@ -2,7 +2,7 @@ import csv
 import numpy as np
 import pandas as pd
 
-rawdata = pd.read_csv("input60NY.csv")
+rawdata = pd.read_csv("citiesdataset-NYDcor-4 (KZ).csv")
 
 for i in range(rawdata.shape[0]):
     if rawdata.iloc[i, rawdata.shape[1] - 1] > 0:
@@ -10,5 +10,6 @@ for i in range(rawdata.shape[0]):
     else:
         rawdata.iloc[i, rawdata.shape[1] - 1] = int(0)
 
+rawdata = rawdata.iloc[:3000]
 
-rawdata.to_csv("input60NY (C).csv", index=False)
+rawdata.to_csv("citiesdataset-NYDcor-4 (CLASS-KZ-3000).csv", index=False)
