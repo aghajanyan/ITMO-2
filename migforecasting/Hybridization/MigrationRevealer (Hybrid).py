697c258d27e37a7364a99ff96b6185f1be7477a6
Albert Aghajanyan <49070457+aghajanyan@users.noreply.github.com>
2024-05-22 10:40:51 +0300

    Hybrid model (ini. stage corr.)

--- migforecasting/Hybridization/MigrationRevealer (Hybrid).py
+++ migforecasting/Hybridization/MigrationRevealer (Hybrid).py
@@ -14,7 +14,7 @@ from sklearn.model_selection import train_test_split
 maxsaldo = 26466
 
 # Получение данных
-rawdata = pd.read_csv("citiesdataset-NYDсor-4.csv")
+rawdata = pd.read_csv("citiesdataset-NYDcor-4.csv")
 
 rawdata = rawdata.sample(frac=1)  # перетасовка
 
@@ -22,33 +22,31 @@ rawdata = rawdata.sample(frac=1)  # перетасовка
 rawdataclass = pd.DataFrame()
 for i in range(rawdata.shape[0]):
     if rawdata.iloc[i, rawdata.shape[1] - 1] > 0:
-        rawdataclass.append(rawdata[i])
-        rawdataclass[i, len(rawdataclass) - 1] = 1
+        rawdataclass = rawdataclass.append(rawdata.iloc[i])
+        rawdataclass.iloc[i, rawdata.shape[1] - 1] = 1
     else:
-        rawdataclass.append(rawdata[i])
-        rawdataclass[i, len(rawdataclass) - 1] = 0
+        rawdataclass = rawdataclass.append(rawdata.iloc[i])
+        rawdataclass.iloc[i, rawdata.shape[1] - 1] = 0
 
 
 # разбиение датасета на входные признаки и выходной результат (сальдо)
-# 1 - для модели регресси, 2 - для модели классификатора
-datasetin1 = np.array(rawdata[rawdata.columns.drop('saldo')])
+# 1 - для модели регресси, 2 - для модели классификатора (входные данные одинаковые)
+datasetin = np.array(rawdata[rawdata.columns.drop('saldo')])
 datasetout1 = np.array(rawdata[['saldo']])
-
-datasetin2 = np.array(rawdataclass[rawdataclass.columns.drop('saldo')])
 datasetout2 = np.array(rawdataclass[['saldo']])
 
 # разбиение на обучающую и тестовую выборку
-trainin1, testin1, trainout1, testout1 = train_test_split(datasetin1, datasetout1, test_size=0.2, random_state=42)
-trainin2, testin2, trainout2, testout2 = train_test_split(datasetin2, datasetout2, test_size=0.2, random_state=42)
+trainin, testin, trainout1, testout1 = train_test_split(datasetin, datasetout1, test_size=0.2, random_state=146)
+trainin, testin, trainout2, testout2 = train_test_split(datasetin, datasetout2, test_size=0.2, random_state=146)
 
 # модель 1
 model1 = RandomForestRegressor(n_estimators=100, random_state=0)
-model1.fit(trainin1, trainout1.ravel())
+model1.fit(trainin, trainout1.ravel())
 predtrainreg = model1.predict(trainin1)
 
 # модель 2
 model2 = RandomForestClassifier(n_estimators=100, random_state=0)
-model2.fit(trainin2, trainout2.ravel())
+model2.fit(trainin, trainout2.ravel())
 predtrainclass = model2.predict(trainin2)
 
 # замена знака прогноза регрессионной модели согласно прогнозу классификатора
@@ -61,8 +59,8 @@ for i in range(len(predtrainreg)):
 errortrain = mean_absolute_error(trainout1, predtrainreg) * maxsaldo
 
 # оценка тестовой выборки
-predtestreg = model1.predict(testin1)
-predtestclass = model1.predict(testin2)
+predtestreg = model1.predict(testin)
+predtestclass = model1.predict(testin)
 
 for i in range(len(predtestreg)):
     if predtestclass[i] == 1:
