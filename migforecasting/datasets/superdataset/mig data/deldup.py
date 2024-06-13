f3fa1b5171b1d4257367a19eb7e0585561d8b2b0
Albert Aghajanyan <49070457+aghajanyan@users.noreply.github.com>
2024-06-13 16:30:39 +0300

    inflow del. duplicates [2]

--- migforecasting/datasets/superdataset/mig data/deldup.py
+++ migforecasting/datasets/superdataset/mig data/deldup.py
@@ -9,4 +9,4 @@ for k in range(11):
     n+=1
     data = pd.read_csv("inflow/inflow "+str(n)+" (allmun).csv")
     data = data.drop_duplicates(subset='oktmo')
-    data.to_csv("inflow/outflow "+str(n)+" (allmun).csv", index=False)
+    data.to_csv("inflow/inflow "+str(n)+" (allmun).csv", index=False)
