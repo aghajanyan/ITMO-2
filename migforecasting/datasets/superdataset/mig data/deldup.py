7c0a376fec07116996af28199dcdc7c0aee375c0
Albert Aghajanyan <49070457+aghajanyan@users.noreply.github.com>
2024-06-14 10:14:40 +0300

    Popsize for all mun (with oktmo)

--- migforecasting/datasets/superdataset/mig data/deldup.py
+++ migforecasting/datasets/superdataset/mig data/deldup.py
@@ -5,12 +5,12 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 n = 2007
-
 for k in range(15):
     examples = []
     n+=1
+
+    #удаление NAN значений из выборки
     data = pd.read_csv("saldo/saldo "+str(n)+" (allmun).csv")
-    # data = data.drop_duplicates(subset='oktmo')
     for i in range(data.shape[0]):
         if data.iloc[i, 3] == data.iloc[i, 3]:
             examples.append(data.iloc[i])
@@ -19,3 +19,10 @@ for k in range(15):
     titles = ['oktmo', 'name', 'year', 'saldo']
     examples = pd.DataFrame(examples, columns=titles)
     examples.to_csv("saldo/saldo "+str(n)+" (allmun).csv", index=False)
+
+    """
+    #удаление повторяющихся значений из выборки
+    data = pd.read_csv("saldo/saldo " + str(n) + " (allmun).csv")
+    data = data.drop_duplicates(subset='oktmo')
+    data.to_csv("saldo/saldo " + str(n) + " (allmun).csv", index=False)
+    """
