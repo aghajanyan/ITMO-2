3c45fe1469f7659331fbec6a254865e229484e06
Albert Aghajanyan <49070457+aghajanyan@users.noreply.github.com>
2024-06-14 10:45:32 +0300

    Saldo for all years in one csv

--- migforecasting/datasets/superdataset/mig data/deldup.py
+++ migforecasting/datasets/superdataset/mig data/deldup.py
@@ -5,24 +5,34 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 n = 2007
+examples = []
 for k in range(15):
-    examples = []
     n+=1
+    data = pd.read_csv("saldo/saldo "+str(n)+" (allmun).csv")
+    for i in range(data.shape[0]):
+        examples.append(data.iloc[i])
 
+examples = np.array(examples)
+titles = ['oktmo', 'name', 'year', 'saldo']
+examples = pd.DataFrame(examples, columns=titles)
+examples.to_csv("saldo (allmun).csv", index=False)
+
+"""
     #удаление NAN значений из выборки
     data = pd.read_csv("saldo/saldo "+str(n)+" (allmun).csv")
     for i in range(data.shape[0]):
         if data.iloc[i, 3] == data.iloc[i, 3]:
             examples.append(data.iloc[i])
-
+    
     examples = np.array(examples)
     titles = ['oktmo', 'name', 'year', 'saldo']
     examples = pd.DataFrame(examples, columns=titles)
     examples.to_csv("saldo/saldo "+str(n)+" (allmun).csv", index=False)
+"""
 
-    """
+"""
     #удаление повторяющихся значений из выборки
     data = pd.read_csv("saldo/saldo " + str(n) + " (allmun).csv")
     data = data.drop_duplicates(subset='oktmo')
     data.to_csv("saldo/saldo " + str(n) + " (allmun).csv", index=False)
-    """
+"""
