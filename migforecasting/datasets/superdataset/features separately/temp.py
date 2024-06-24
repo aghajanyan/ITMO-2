808df4ee69a78ea97b3fb95c1bd3193b213d19bb
Albert Aghajanyan <49070457+aghajanyan@users.noreply.github.com>
2024-06-24 15:42:33 +0300

    Funds for all mun

--- migforecasting/datasets/superdataset/features separately/temp.py
+++ migforecasting/datasets/superdataset/features separately/temp.py
@@ -5,11 +5,12 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-data = pd.read_csv("theatres (allmun).csv")
+data = pd.read_csv("naturesecure (allmun).csv")
 
+print('whats up')
 # удалить строку
-data = data.drop([0])
+#data = data.drop([0])
 #удалить столбец
 #data = data[rawdata.columns.drop('index')]
 
-data.to_csv("theatres (allmun).csv", index=False)
+#data.to_csv("theatres (allmun).csv", index=False)
