1ac5b97a4d0fc4e42d687f8095d5ae4eb7509997
Albert Aghajanyan <49070457+aghajanyan@users.noreply.github.com>
2024-06-27 09:15:40 +0300

    foodseats for all mun (corr.)

--- migforecasting/datasets/superdataset/features separately/temp.py
+++ migforecasting/datasets/superdataset/features separately/temp.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-data = pd.read_csv("shoparea (allmun).csv")
+data = pd.read_csv("foodseats (allmun).csv")
 
 data = data.sort_values(by=['oktmo', 'year'])
 corrset = []
@@ -22,13 +22,13 @@ while i < data.shape[0]:
                 corrset.pop()
     i+=1
 
-titles = ['oktmo', 'name', 'year', 'shoparea']
+titles = ['oktmo', 'name', 'year', 'foodseats']
 
 corrset = pd.DataFrame(corrset, columns=titles)
 
 corrset = corrset.drop_duplicates()
 
-corrset.to_csv("shoparea (allmun).csv", index=False)
+corrset.to_csv("foodseats (allmun).csv", index=False)
 
 
 print('whats up')
