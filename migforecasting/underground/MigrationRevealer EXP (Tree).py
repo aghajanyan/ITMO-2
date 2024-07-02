7eab1ec4dfd0413a212f9fe89262ad90fd6af2bf
Albert Aghajanyan <49070457+aghajanyan@users.noreply.github.com>
2024-07-02 15:28:12 +0300

    prodnorm (beef) test with mae

--- migforecasting/underground/MigrationRevealer EXP (Tree).py
+++ migforecasting/underground/MigrationRevealer EXP (Tree).py
@@ -11,7 +11,7 @@ from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 
 
-rawdata = pd.read_csv("datasets/superdataset-03.csv")
+rawdata = pd.read_csv("datasets/superdataset-04.csv")
 
 resulttest = []
 resulttrain = []
