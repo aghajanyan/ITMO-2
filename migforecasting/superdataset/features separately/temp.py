aac3eb39523ea4502d7eb44d0c99b8884ebae541
Albert Aghajanyan <49070457+aghajanyan@users.noreply.github.com>
2024-08-14 11:49:33 +0300

    new feature - visiblecompanies

--- migforecasting/superdataset/features separately/temp.py
+++ migforecasting/superdataset/features separately/temp.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-data = pd.read_csv("badcompanies (allmun).csv")
+data = pd.read_csv("visiblecompanies (allmun).csv")
 
 data = data.sort_values(by=['oktmo', 'year'])
 corrset = []
@@ -22,13 +22,13 @@ while i < data.shape[0]:
                 corrset.pop()
     i+=1
 
-titles = ['oktmo', 'name', 'year', 'badcompanies']
+titles = ['oktmo', 'name', 'year', 'visiblecompanies']
 
 corrset = pd.DataFrame(corrset, columns=titles)
 
 corrset = corrset.drop_duplicates()
 
-corrset.to_csv("badcompanies (allmun).csv", index=False)
+corrset.to_csv("visiblecompanies (allmun).csv", index=False)
 
 
 print('whats up')
