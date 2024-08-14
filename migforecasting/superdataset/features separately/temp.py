c8a38f1abc7d56e0c6bc23621b7bc0da86fa096b
Albert Aghajanyan <49070457+aghajanyan@users.noreply.github.com>
2024-08-14 10:23:40 +0300

    new feature - badcompanies

--- migforecasting/superdataset/features separately/temp.py
+++ migforecasting/superdataset/features separately/temp.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-data = pd.read_csv("goodcompanies (allmun).csv")
+data = pd.read_csv("badcompanies (allmun).csv")
 
 data = data.sort_values(by=['oktmo', 'year'])
 corrset = []
@@ -22,13 +22,13 @@ while i < data.shape[0]:
                 corrset.pop()
     i+=1
 
-titles = ['oktmo', 'name', 'year', 'goodcompanies']
+titles = ['oktmo', 'name', 'year', 'badcompanies']
 
 corrset = pd.DataFrame(corrset, columns=titles)
 
 corrset = corrset.drop_duplicates()
 
-corrset.to_csv("goodcompanies (allmun).csv", index=False)
+corrset.to_csv("badcompanies (allmun).csv", index=False)
 
 
 print('whats up')
