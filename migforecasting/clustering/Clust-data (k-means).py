5434c628475e023f2c98516b78decabe3605ed30
Albert Aghajanyan <49070457+aghajanyan@users.noreply.github.com>
2024-09-30 14:29:14 +0300

    k-means ver. 0.2 (avg. error for clusters)

--- migforecasting/clustering/Clust-data (k-means).py
+++ migforecasting/clustering/Clust-data (k-means).py
@@ -10,12 +10,30 @@ from sklearn.metrics import silhouette_score
 
 data = pd.read_csv("superdataset-24 2022-clust.csv")
 
-data = data.sample(frac=1)  # перетасовка
+error = []
+tmper = []
+N = range(2, 11)    # количество кластеров
+for i in range(10):     # цикл для вычисления средней ошибки для конкретного кол-ва кластеров
+    tmper = []
+    data = data.sample(frac=1)  # перетасовка
+    for n in N:
+        # модель кластеризации и вычисление ошибки
+        clust_model = KMeans(n_clusters=n, random_state=None, n_init='auto')
+        clust_model.fit(data)
+        tmper.append(silhouette_score(data, clust_model.labels_, metric='euclidean'))
 
-clust_model = KMeans(n_clusters=4, random_state=None, n_init='auto')
+    for j, m in enumerate(tmper):
+        if i == 0:
+            error.append(m)
+        else:
+            error[j]+=m
 
-clust_model.fit(data)
+for i in range(len(error)):
+    error[i] = error[i] / 10
 
-error = silhouette_score(data, clust_model.labels_, metric='euclidean')
+plt.plot(N, error)
+plt.xlabel("Number of clusters")
+plt.ylabel("Error")
+plt.show()
 
 print(error)
