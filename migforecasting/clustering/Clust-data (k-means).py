b32265cfed55df38d1671458a77be4b10fae0032
Albert Aghajanyan <49070457+aghajanyan@users.noreply.github.com>
2024-09-30 13:51:44 +0300

    k-means ver. 0.1 (first try)

--- migforecasting/clustering/Clust-data (k-means).py
+++ migforecasting/clustering/Clust-data (k-means).py
@@ -5,4 +5,17 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
 
+
+data = pd.read_csv("superdataset-24 2022-clust.csv")
+
+data = data.sample(frac=1)  # перетасовка
+
+clust_model = KMeans(n_clusters=4, random_state=None, n_init='auto')
+
+clust_model.fit(data)
+
+error = silhouette_score(data, clust_model.labels_, metric='euclidean')
+
+print(error)
