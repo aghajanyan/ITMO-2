e6fcfc5e9ae4ee8baadcdee51a6fb8c1e103b22d
Albert Aghajanyan <49070457+aghajanyan@users.noreply.github.com>
2024-10-02 10:45:42 +0300

    Create Clust-error (k-means).py

---
+++ migforecasting/clustering/Clust-error (k-means).py
@@ -0,0 +1,36 @@
+import csv
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+
+data = pd.read_csv("superdataset-24 2022-clust.csv")
+
+error = []
+tmper = []
+N = range(2, 11)    # количество кластеров
+for i in range(10):     # цикл для вычисления средней ошибки для конкретного кол-ва кластеров
+    tmper = []
+    data = data.sample(frac=1)  # перетасовка
+    for n in N:
+        # модель кластеризации и вычисление ошибки
+        clust_model = KMeans(n_clusters=n, random_state=None, n_init='auto')
+        clust_model.fit(data)
+        tmper.append(silhouette_score(data, clust_model.labels_, metric='euclidean'))
+
+    for j, m in enumerate(tmper):
+        if i == 0:
+            error.append(m)
+        else:
+            error[j]+=m
+
+for i in range(len(error)):
+    error[i] = error[i] / 10
+
+plt.plot(N, error)
+plt.xlabel("Number of clusters")
+plt.ylabel("Error")
+plt.show()
