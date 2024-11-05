117859e396258be0c838acc0b3814b94950e1d7a
Albert Aghajanyan <49070457+aghajanyan@users.noreply.github.com>
2024-11-05 18:01:25 +0300

    Recommsys ver. 0.3.1 (normalization)

--- migforecasting/clustering/recommendation system/Recommsys.py
+++ migforecasting/clustering/recommendation system/Recommsys.py
@@ -4,10 +4,20 @@ import random
 import matplotlib.pyplot as plt
 import pandas as pd
 
+def normpersoul(tonorm):
+    for k in range(len(tonorm)):
+        for col in tonorm.iloc[:, 7:]:
+            if col != 'avgsalary' and col != 'livarea' and col != 'roadslen' and col != 'hospitals':
+                for m in range(len(tonorm)):
+                    #tonorm = tonorm.astype({col: float})
+                    x = float(tonorm.iloc[m][col] / tonorm.iloc[m]['popsize'])
+                    tonorm.iloc[0, 7] = x
 
 medians = pd.read_csv("medians.csv")
 
 input = pd.read_excel("input.xlsx")
+
+normpersoul(input)
 
 changes = []
 tmp = []
@@ -28,7 +38,7 @@ changes = pd.DataFrame(changes, columns=features)
 changes = changes.transpose()
 
 ax = changes.plot.barh()
-ax.set_title("Сбалансированный вектор развития "+ input.iloc[0, 2] +" относительно лучшей группы мун. образований")
+ax.set_title("Сбалансированный вектор развития "+ input.iloc[0]['name'] +" относительно лучшей группы мун. образований")
 ax.set_xlabel('Во сколько раз необходимо улучшить')
 ax.set_ylabel('Социально-экономические индикаторы')
 plt.legend(input['profile'])
