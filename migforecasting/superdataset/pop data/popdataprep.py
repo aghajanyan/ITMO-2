c969f4fbb1ed6f1f26e91b9fe7915f1fea416f83
Albert Aghajanyan <49070457+aghajanyan@users.noreply.github.com>
2025-02-04 18:14:54 +0300

    popdataprep.py ver 0.3

--- migforecasting/superdataset/pop data/popdataprep.py
+++ migforecasting/superdataset/pop data/popdataprep.py
@@ -12,7 +12,10 @@ with open('LO outflow.csv', 'r', newline='', encoding='utf-8') as csvfile:
         row = next(reader)
         data.append(np.array(row))
 """
-# для миграции
+
+gender = 'Male'
+
+# загрузка и первичная обработка входного файла файла
 with open('pop23.csv', 'r', newline='', encoding='utf-8') as csvfile:
     reader = csv.reader(csvfile, delimiter=';')
     for i in range(1300):
@@ -28,30 +31,13 @@ data = np.array(data)
 data = pd.DataFrame(data, columns=data[0])
 
 data = data.sort_values(by=['oktmo', 'vozr'])
-"""
-newdata = []
-for i in range(len(data)):
-    if len(list(data.iloc[i]['vozr'])) < 3:
-        newdata.append(data.iloc[i])
-
-newdata = np.array(newdata)
-newdata = pd.DataFrame(newdata, columns=data[0])
-
-for index, row in data.iterrows():
-    try:
-        tmp = int(row['vozr'])
-    except ValueError:
-        data = data.drop(index)
-
-"""
-
-cols = list(['oktmo', 'municipality', 'year', 'indicator_value']) + list(range(0, 70))
-
 
 # убрать когорты и оставить только данные за конкретные года (0, 1, ..., 69)
 data = data[data['vozr'].str.isdigit()]
 
 data = data.astype({"vozr": int})
+data = data.astype({"indicator_value": float})
+data = data.astype({"indicator_value": int})
 data = data.sort_values(by=['oktmo', 'vozr'])
 
 newdata = data[['oktmo', 'municipality', 'year', 'vozr', 'indicator_value']]
@@ -59,50 +45,38 @@ newdata = data[['oktmo', 'municipality', 'year', 'vozr', 'indicator_value']]
 tmp = []
 final = []
 i = 0
-while i < len(newdata) - 1:
-    if newdata.iloc[i]['oktmo'] != newdata.iloc[i+1]['oktmo']:
+while i < len(newdata):
+    if i != len(newdata) - 1:
+        if newdata.iloc[i]['oktmo'] != newdata.iloc[i+1]['oktmo']:
+            tmp.append(newdata.iloc[i]['indicator_value'])
+            final.append(np.array(tmp))
+            tmp.clear()
+        else:
+            if newdata.iloc[i]['vozr'] != newdata.iloc[i+1]['vozr']:
+                if len(tmp) != 0:
+                    tmp.append(newdata.iloc[i]['indicator_value'])
+                else:
+                    tmp.append(newdata.iloc[i]['oktmo'])
+                    tmp.append(newdata.iloc[i]['municipality'])
+                    tmp.append(newdata.iloc[i]['year'])
+                    tmp.append(gender)
+                    tmp.append(newdata.iloc[i]['indicator_value'])
+            else:
+                if newdata.iloc[i]['indicator_value'] < newdata.iloc[i+1]['indicator_value']:
+                    tmp.append(newdata.iloc[i]['indicator_value'])
+                    i+=1
+                else:
+                    tmp.append(newdata.iloc[i+1]['indicator_value'])
+                    i+=1
+        i += 1
+    else:
         tmp.append(newdata.iloc[i]['indicator_value'])
         final.append(np.array(tmp))
         tmp.clear()
-    else:
-        if newdata.iloc[i]['vozr'] != newdata.iloc[i+1]['vozr']:
-            if len(tmp) != 0:
-                tmp.append(newdata.iloc[i]['indicator_value'])
-            else:
-                tmp.append(newdata.iloc[i]['oktmo'])
-                tmp.append(newdata.iloc[i]['indicator_value'])
-        else:
-            if newdata.iloc[i]['indicator_value'] < newdata.iloc[i+1]['indicator_value']:
-                tmp.append(newdata.iloc[i]['indicator_value'])
-                i+=1
-            else:
-                tmp.append(newdata.iloc[i+1]['indicator_value'])
-                i+=1
-    i += 1
+        i += 1
 
+final = np.array(final)
+cols = list(['oktmo', 'name', 'year', 'gender']) + list(range(0, 70))
+final = pd.DataFrame(final, columns=cols)
 
-
-
-prepdata = []
-tmp = []
-b = 0
-oktmo = data[0, 11]
-name = data[0, 9]
-year = data[0, 13]
-total = 0
-while b < len(data):
-    tmp.append(data[b, 12])
-    tmp.append(data[b, 11])
-    tmp.append(data[b, 14])
-    tmp.append(data[b, 15])
-    prepdata.append(np.array(tmp))
-    tmp.clear()
-    b+=1
-
-titles = ['oktmo', 'name', 'year', 'inflow']
-
-prepdata = pd.DataFrame(prepdata, columns=titles)
-
-prepdata = prepdata.drop_duplicates()
-
-prepdata.to_csv("inflow reg 2021.csv", index=False)
+final.to_csv("agestruct "+gender+" 2023.csv", index=False)
