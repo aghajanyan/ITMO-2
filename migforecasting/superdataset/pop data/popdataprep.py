5d11de6feb828b51b7042b387504f8fc3e5d6696
Albert Aghajanyan <49070457+aghajanyan@users.noreply.github.com>
2025-02-04 16:49:02 +0300

    popdataprep.py ver 0.2

--- migforecasting/superdataset/pop data/popdataprep.py
+++ migforecasting/superdataset/pop data/popdataprep.py
@@ -15,9 +15,13 @@ with open('LO outflow.csv', 'r', newline='', encoding='utf-8') as csvfile:
 # для миграции
 with open('pop23.csv', 'r', newline='', encoding='utf-8') as csvfile:
     reader = csv.reader(csvfile, delimiter=';')
-    for row in reader:
+    for i in range(1300):
+        row = next(reader)
+        if row[4] == 'Всего' or i == 0:
+            data.append(np.array(row))
+    #for row in reader:
         #if row[4] == 'Всего':
-        data.append(np.array(row))
+            #data.append(np.array(row))
 
 data = np.array(data)
 
@@ -41,11 +45,43 @@ for index, row in data.iterrows():
 
 """
 
+cols = list(['oktmo', 'municipality', 'year', 'indicator_value']) + list(range(0, 70))
+
+
 # убрать когорты и оставить только данные за конкретные года (0, 1, ..., 69)
 data = data[data['vozr'].str.isdigit()]
 
 data = data.astype({"vozr": int})
 data = data.sort_values(by=['oktmo', 'vozr'])
+
+newdata = data[['oktmo', 'municipality', 'year', 'vozr', 'indicator_value']]
+
+tmp = []
+final = []
+i = 0
+while i < len(newdata) - 1:
+    if newdata.iloc[i]['oktmo'] != newdata.iloc[i+1]['oktmo']:
+        tmp.append(newdata.iloc[i]['indicator_value'])
+        final.append(np.array(tmp))
+        tmp.clear()
+    else:
+        if newdata.iloc[i]['vozr'] != newdata.iloc[i+1]['vozr']:
+            if len(tmp) != 0:
+                tmp.append(newdata.iloc[i]['indicator_value'])
+            else:
+                tmp.append(newdata.iloc[i]['oktmo'])
+                tmp.append(newdata.iloc[i]['indicator_value'])
+        else:
+            if newdata.iloc[i]['indicator_value'] < newdata.iloc[i+1]['indicator_value']:
+                tmp.append(newdata.iloc[i]['indicator_value'])
+                i+=1
+            else:
+                tmp.append(newdata.iloc[i+1]['indicator_value'])
+                i+=1
+    i += 1
+
+
+
 
 prepdata = []
 tmp = []
