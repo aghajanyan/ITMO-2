ea1256b0dafc3cb6464e0e01de7d3365c75ebdd4
Albert Aghajanyan <49070457+aghajanyan@users.noreply.github.com>
2025-02-07 16:25:38 +0300

    Update popconnecter.py

--- migforecasting/superdataset/pop data/popconnecter.py
+++ migforecasting/superdataset/pop data/popconnecter.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-male = pd.read_csv("agestruct male 2023.csv")
-female = pd.read_csv("agestruct female 2023.csv")
+male = pd.read_csv("agestruct male 2022.csv")
+female = pd.read_csv("agestruct female 2022.csv")
 
 mf = [male, female]
 mf = pd.concat(mf)
@@ -14,6 +14,7 @@ mf = pd.concat(mf)
 mf = mf.sort_values(by=['oktmo', 'gender'])
 mf = mf.reset_index(drop=True)
 
+# каждой твари по паре, инчае дроп
 a = 0
 while a < len(mf) - 1:
     if mf.iloc[a]['oktmo'] != mf.iloc[a + 1]['oktmo']:
@@ -21,4 +22,11 @@ while a < len(mf) - 1:
     else:
         a += 2
 
-mf.to_csv("agestruct 2023.csv", index=False)
+# если значение 0 (замена nan'ов), приравнять кол-во предыдущего возраста
+for i in range(len(mf)):
+    for j in range(0, 70):
+        if mf.iloc[i][''+str(j)+''] == 0:
+            index = mf.columns.get_loc(''+str(j)+'')
+            mf.iloc[i, index] = mf.iloc[i, index - 1]
+
+mf.to_csv("agestruct 2022.csv", index=False)
