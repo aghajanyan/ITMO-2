9e6edf292c20db3347d170aaaa04c6e78aaa0219
Albert Aghajanyan <49070457+aghajanyan@users.noreply.github.com>
2025-02-10 10:42:58 +0300

    Update popconnecter.py

--- migforecasting/superdataset/pop data/popconnecter.py
+++ migforecasting/superdataset/pop data/popconnecter.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-male = pd.read_csv("agestruct male 2022.csv")
-female = pd.read_csv("agestruct female 2022.csv")
+male = pd.read_csv("agestruct male 2017.csv")
+female = pd.read_csv("agestruct female 2017.csv")
 
 mf = [male, female]
 mf = pd.concat(mf)
@@ -22,11 +22,20 @@ while a < len(mf) - 1:
     else:
         a += 2
 
-# если значение 0 (замена nan'ов), приравнять кол-во предыдущего возраста
+# если значение 0 (замена nan'ов), приравнять кол-во предыдущего возраста (либо следующего, если новорожденные)
 for i in range(len(mf)):
     for j in range(0, 70):
-        if mf.iloc[i][''+str(j)+''] == 0:
-            index = mf.columns.get_loc(''+str(j)+'')
-            mf.iloc[i, index] = mf.iloc[i, index - 1]
+        if j != 0:
+            if mf.iloc[i][''+str(j)+''] == 0:
+                index = mf.columns.get_loc(''+str(j)+'')
+                mf.iloc[i, index] = mf.iloc[i, index - 1]
+        else:
+            if mf.iloc[i]['' + str(j) + ''] == 0:
+                index = mf.columns.get_loc(''+str(j)+'')
+                if mf.iloc[i, index + 1] != 0:
+                    mf.iloc[i, index] = mf.iloc[i, index + 1]
+                else:
+                    mf.iloc[i, index] = mf.iloc[i, index + 2]
 
-mf.to_csv("agestruct 2022.csv", index=False)
+
+mf.to_csv("agestruct 2017.csv", index=False)
