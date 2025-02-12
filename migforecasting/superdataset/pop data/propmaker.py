105c3f0f21243b60896f04dd6e7b1f64b0f66c44
Albert Aghajanyan <49070457+aghajanyan@users.noreply.github.com>
2025-02-12 10:29:06 +0300

    propmaker.py (loop)

--- migforecasting/superdataset/pop data/propmaker.py
+++ migforecasting/superdataset/pop data/propmaker.py
@@ -4,34 +4,37 @@ import random
 import matplotlib.pyplot as plt
 import pandas as pd
 
-y = 2023
-data = pd.read_csv("agestruct (per cohort)/agestruct "+ str(y) +" (cohort).csv")
 
-allprop = []
-tmp = []
+for y in range(2011, 2024):
+    data = pd.read_csv("agestruct (per cohort)/agestruct "+ str(y) +" (cohort).csv")
 
-cols = ['0-4', '5-9', '10-14', '15-19', '20-24',
-        '25-29', '30-34', '35-39', '40-44', '45-49',
-        '50-54', '55-59', '60-64', '65-69']
+    cols = ['0-4', '5-9', '10-14', '15-19', '20-24',
+            '25-29', '30-34', '35-39', '40-44', '45-49',
+            '50-54', '55-59', '60-64', '65-69']
 
-# общее количество людей (сумма всех когорт)
-data['total'] = data.iloc[:, 4: data.shape[1]].sum(axis=1)
+    # общее количество людей (сумма всех когорт)
+    data['total'] = data.iloc[:, 4: data.shape[1]].sum(axis=1)
 
-# вычисление доли в когорте от общего числа людей
-for i in range(len(data)):
-    for j in cols:
-        prop = data.iloc[i][j] / data.iloc[i]['total']
-        tmp.append(np.float16(prop))
-    allprop.append(np.array(tmp))
-    tmp.clear()
+    allprop = []
+    tmp = []
+    # вычисление доли в когорте от общего числа людей
+    for i in range(len(data)):
+        for j in cols:
+            prop = data.iloc[i][j] / data.iloc[i]['total']
+            tmp.append(np.float16(prop))
+        allprop.append(np.array(tmp))
+        tmp.clear()
 
-# создание таблицы
-allprop = np.array(allprop)
-allprop = pd.DataFrame(allprop, columns=cols)
-allprop[['oktmo', 'name', 'gender', 'year']] = data[['oktmo', 'name', 'gender', 'year']]
+    # создание таблицы
+    allprop = np.array(allprop)
+    allprop = pd.DataFrame(allprop, columns=cols)
+    allprop[['oktmo', 'name', 'gender', 'year']] = data[['oktmo', 'name', 'gender', 'year']]
 
-# порядок столбцов в таблице
-cols = ['oktmo', 'name', 'gender', 'year'] + cols
-allprop = allprop[cols]
+    # порядок столбцов в таблице
+    cols = ['oktmo', 'name', 'gender', 'year'] + cols
+    allprop = allprop[cols]
+
+    allprop.to_csv("agestruct (prop)/agestruct "+ str(y) +" (prop).csv", index=False)
+    print('Итерация № ', y)
 
 print('Done')
