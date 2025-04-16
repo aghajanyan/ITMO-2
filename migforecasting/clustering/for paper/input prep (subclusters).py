c2b1d0e3e96291a8bc5671b0a7fcbbd7a3b68634
Albert Aghajanyan <49070457+aghajanyan@users.noreply.github.com>
2025-04-16 10:46:37 +0300

    Update input prep (subclusters).py

--- migforecasting/clustering/for paper/input prep (subclusters).py
+++ migforecasting/clustering/for paper/input prep (subclusters).py
@@ -8,12 +8,18 @@ import pandas as pd
 subclusters = pd.read_excel("medians of subclusters.xlsx")
 data = pd.read_excel("input to change.xlsx")
 
+changes = []
 for i in range(len(data)):
     for j in range((len(subclusters))):
         if data.iloc[i]['clust'] == subclusters.iloc[j]['clust']:
             for k in range(data.shape[1]):
                 if data.iloc[i][k] < subclusters.iloc[j][k]:
-                    data.iloc[i][k] = subclusters.iloc[j][k]
+                    changes.append(subclusters.iloc[j][k])
+                else:
+                    changes.append(data.iloc[i][k])
 
+features = data.columns
+changes = np.array(changes)
+changes = pd.DataFrame(changes, columns=features)
 
-data.to_excel("input to change.xlsx")
+changes.to_excel("input with changes.xlsx")
