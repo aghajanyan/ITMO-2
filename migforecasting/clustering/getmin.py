f14c4005a10dea1effd5b1eb101f6c3152b94285
Albert Aghajanyan <49070457+aghajanyan@users.noreply.github.com>
2025-04-23 09:46:44 +0300

    Update getmin.py

--- migforecasting/clustering/getmin.py
+++ migforecasting/clustering/getmin.py
@@ -4,6 +4,15 @@ import random
 import matplotlib.pyplot as plt
 import pandas as pd
 
+# возвращение в абсолютную форму
+def backtoabs(normdata):
+    # факторы для нормирования
+    features = ['avgemployers', 'shoparea', 'foodseats', 'retailturnover', 'sportsvenue', 'servicesnum',
+                'roadslen', 'livestock', 'harvest', 'agrprod', 'hospitals', 'beforeschool', 'factoriescap']
+
+    for a in features:
+        normdata[a] = normdata[a] * normdata['popsize']
+
 
 data = pd.read_excel("Clusters.xlsx", sheet_name=None)
 
@@ -11,6 +20,7 @@ tmp = []
 mindata = []
 for cluster in data:
     positive = data[cluster][data[cluster]['saldo'] > 0]
+    backtoabs(positive)
     tmp.append(cluster)
     for col in positive.columns[5:]:
         tmp.append(positive[col].min())
@@ -21,5 +31,7 @@ features = list(data['Cluster 0'].columns[5:])
 features.insert(0, 'clust')
 mindata = np.array(mindata)
 mindata = pd.DataFrame(mindata, columns=features)
+
+mindata.to_excel("mindata (abs).xlsx", index=False)
 
 print('ok')
