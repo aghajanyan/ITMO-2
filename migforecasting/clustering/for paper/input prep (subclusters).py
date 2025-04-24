2934c0eb7958f758dbb3ce677a1c240658ee0ea4
Albert Aghajanyan <49070457+aghajanyan@users.noreply.github.com>
2025-04-24 10:46:54 +0300

    Update input prep (subclusters).py

--- migforecasting/clustering/for paper/input prep (subclusters).py
+++ migforecasting/clustering/for paper/input prep (subclusters).py
@@ -6,6 +6,7 @@ import pandas as pd
 
 
 subclusters = pd.read_excel("medians of subclusters.xlsx")
+scdiff = pd.read_excel("scdiff.xlsx")
 data = pd.read_excel("input to change.xlsx")
 
 changes = []
@@ -15,7 +16,11 @@ for i in range(len(data)):
         if data.iloc[i]['clust'] == subclusters.iloc[j]['clust']:
             for k in range(data.shape[1]):
                 if data.iloc[i][k] < subclusters.iloc[j][k]:
-                    tmp.append(subclusters.iloc[j][k])
+                    if subclusters.iloc[j][k] / data.iloc[i][k] < 2:
+                        tmp.append(subclusters.iloc[j][k]) # замена на медиану (если разница от неё меньше 100%)
+                    else:
+                        # увеличение на сред. процент разницы между медианами полож. и отриц. субкластера
+                        tmp.append(data.iloc[i][k] + (data.iloc[i][k] * scdiff.iloc[j][k]))
                 else:
                     tmp.append(data.iloc[i][k])
             changes.append(tmp)
