8ab5922a6e2d1afcd9ab1a3ae8375fbbc4d39340
Albert Aghajanyan <49070457+aghajanyan@users.noreply.github.com>
2025-04-30 14:22:11 +0300

    Update getmin.py

--- migforecasting/clustering/getmin.py
+++ migforecasting/clustering/getmin.py
@@ -19,19 +19,20 @@ data = pd.read_excel("Clusters.xlsx", sheet_name=None)
 tmp = []
 mindata = []
 for cluster in data:
-    positive = data[cluster][data[cluster]['saldo'] > 0]
+    #positive = data[cluster][data[cluster]['saldo'] > 0]
+    positive = data[cluster]
     backtoabs(positive)
     tmp.append(cluster)
-    for col in positive.columns[5:]:
-        tmp.append(positive[col].min())
+    for col in positive.columns[4:]:
+        tmp.append(positive[col].median())
     mindata.append(tmp)
     tmp = []
 
-features = list(data['Cluster 0'].columns[5:])
+features = list(data['Cluster 0'].columns[4:])
 features.insert(0, 'clust')
 mindata = np.array(mindata)
 mindata = pd.DataFrame(mindata, columns=features)
 
-mindata.to_excel("mindata (abs).xlsx", index=False)
+mindata.to_excel("mediandata (abs).xlsx", index=False)
 
 print('ok')
