623fbbe0f2c29fe8462ec24629e31a96d89513c0
Albert Aghajanyan <49070457+aghajanyan@users.noreply.github.com>
2025-07-04 16:00:18 +0400

    test-23: hybrid model (mse)

--- migforecasting/underground/MigrationRevealer EXP (hybrid-Extrapol).py
+++ migforecasting/underground/MigrationRevealer EXP (hybrid-Extrapol).py
@@ -61,14 +61,14 @@ for k in range(n):
 
     # вычисление ошибки
     predsum = modelsum.predict(testin)
-    errorsum = mean_squared_error(testout * maxsaldosum, predsum * maxsaldosum)
+    errorsum = mean_absolute_error(testout * maxsaldosum, predsum * maxsaldosum)
 
     # вычисление ошибки на своём датасете
     predtest = modelone.predict(testin2)
-    testerror = mean_squared_error(testout2 * maxsaldoone, predtest * maxsaldoone)
+    testerror = mean_absolute_error(testout2 * maxsaldoone, predtest * maxsaldoone)
 
     predtest2 = modeltwo.predict(testin3)
-    testerror2 = mean_squared_error(testout3 * maxsaldotwo, predtest2 * maxsaldotwo)
+    testerror2 = mean_absolute_error(testout3 * maxsaldotwo, predtest2 * maxsaldotwo)
 
     # перенормализация тестовой выборки под другую модель
     normsum = pd.read_csv("datasets/fornorm 24-f 2Ysum.csv")
@@ -95,7 +95,7 @@ for k in range(n):
     predone = predone * maxsaldoone
     predtwo = predtwo * maxsaldotwo
     predextra = predone + predtwo
-    errorextra = mean_squared_error(testout * maxsaldosum, predextra)
+    errorextra = mean_absolute_error(testout * maxsaldosum, predextra)
 
     # запись ошибки
     testresultsum.append(errorsum)
