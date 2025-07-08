23ea1d24b196b5c22c589ea03f84db5742f6a6e3
Albert Aghajanyan <49070457+aghajanyan@users.noreply.github.com>
2025-07-08 19:58:55 +0400

    test-23: extrapol 2.0

--- migforecasting/underground/MigrationRevealer EXP (Extrapol 2.0).py
+++ migforecasting/underground/MigrationRevealer EXP (Extrapol 2.0).py
@@ -44,20 +44,20 @@ for k in range(n):
 
     # вычисление ошибки
     predsum = modelsum.predict(testin)
-    errorsum = mean_squared_error(testout * maxsaldosum, predsum * maxsaldosum)
+    errorsum = mean_absolute_error(testout * maxsaldosum, predsum * maxsaldosum)
 
     # вычисление ошибки на своём датасете
-    predtest = modelone.predict(testin)
-    testerror = r2_score(testout2 * maxsaldoone, predtest * maxsaldoone)
+    #predtest = modelone.predict(testin)
+    #testerror = mean_absolute_error(testout2 * maxsaldoone, predtest * maxsaldoone)
 
     # вычисление ошибки проноза с экстраполяцией
     predone = modelone.predict(testin)
     predone = predone * maxsaldoone
     predextra = predone * 2
-    errorextra = mean_squared_error(testout * maxsaldosum, predextra)
+    errorextra = mean_absolute_error(testout * maxsaldosum, predextra)
 
     # запись ошибки
-    testresultsum.append(testerror)
+    testresultsum.append(errorsum)
     testresultextra.append(errorextra)
 
     print('Итерация: ' + str(k))
